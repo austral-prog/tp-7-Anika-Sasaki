dca4da3f6afa2befcc78ce3422b3ddd48d67d46f
Anika-Sasaki <162721789+Anika-Sasaki@users.noreply.github.com>
2024-05-18 00:54:55 -0300

    Entrego loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,19 @@
 def enumerate_list(list):
-    return list
-
+    new_list = []
+    index = 0
+    for element in list:
+        if element != "":
+            new = f"{index}. " + element
+            index += 1
+            new_list.append(new)
+    return new_list
 
 def enumerate_backwards(list):
-    return list
+    new_list = []
+    index = 0
+    for element in list:
+        if element != "":
+            new = f"{index}. " + str(element[::-1])
+            index += 1
+            new_list.append(new)
+    return new_list
